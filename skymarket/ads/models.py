2db95f9f21e481556519b4d8c3866d683488bf91
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2024-05-02 23:19:52 +0500

    ads models initial

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,38 @@
 from django.conf import settings
 from django.db import models
 
+NULLABLE = {
+    "null": True,
+    "blank": True
+}
+
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=100, verbose_name='название')
+    price = models.PositiveIntegerField(verbose_name='цена')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    image = models.ImageField(upload_to='media', **NULLABLE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="пользователь", **NULLABLE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="время создания")
+
+    def __str__(self):
+        return f"{self.title} - {self.price}"
+
+    class Meta:
+        verbose_name = "объявление"
+        verbose_name_plural = "объявления"
+        ordering = ["-created_at"]
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.TextField(verbose_name="текст отзыва")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="пользователь", **NULLABLE)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, verbose_name="объявление")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="время создания")
+
+    def __str__(self):
+        return f"{self.ad}: {self.text}"
+
+    class Meta:
+        verbose_name = "отзыв"
+        verbose_name_plural = "отзывы"
